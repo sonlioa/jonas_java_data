8577ca888c662e364c6b29d2f6839ddf322ec6f1
Jonas Liao <sonlioa@gmail.com>
2024-04-05 22:27:23 +0800

    add datatypepractice

---
+++ datatypevariable.java
@@ -0,0 +1,21 @@
+public class datatypevariable{
+	public static void main(String[] args){
+		//print out all kind of data type
+		//interger, derfault is intergrate
+		int x;
+		x=3;
+		System.out.println(x);
+		x=120;
+		System.out.println(120);
+		// long interger, digit add L on behind
+		long y=2000000000000L;
+		System.out.println(y);
+		double m=3.14123;
+		System.out.println(m);
+		float n=3.13159268F;
+		System.out.println(n);
+		System.out.println(false);
+		System.out.println('a');
+		System.out.println("hallohere");
+	}
+}
