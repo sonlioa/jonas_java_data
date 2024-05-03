b5229068703fe9b0da1f1bdda8c5bc1eec8da11d
Jonas Liao <sonlioa@gmail.com>
2024-05-03 16:16:20 +0800

    add array_ubung

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array
+// Go through each of the Array indexes, from 0 to 9.
+for (int i = 0; i < 10; i++) {
+    // Access and print what's at the i'th index.
+    System.out.println(squareNumbers[i]);
+}
+
+// Will print:
+// 1
+// 4
+// 9
+// 16
+// 25
+// 36
+// 49
+// 64
+// 81
+// 100
